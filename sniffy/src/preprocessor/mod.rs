a48cb0a1e0b6511b1fa8987aa6b639947fb6fefd
Brady Bangasser <bradybbangasser@gmail.com>
2024-12-02 08:12:07 -0600

    Started spooling function on preprocessor

--- sniffy/src/preprocessor/mod.rs
+++ sniffy/src/preprocessor/mod.rs
@@ -1,22 +1,32 @@
+use std::thread;
 
 pub mod preprocessor {
     use crate::types::types::Inmate;
     use std::collections::VecDeque;
 
+    const MAX_THREADS: u8 = 4;
+
     pub struct Preprocessor {
-        pub output: VecDeque<Inmate>;
-        threads
+        pub output: VecDeque<Inmate>,
+        threads: Vec<std::thread::JoinHandle<()>>,
     }
 
     impl Preprocessor {
-        pub fn new() -> Preprocessor {
-            return Preprocessor {
+        pub fn new() -> &mut Preprocessor {
+            let mut pp = Preprocessor {
                 output: VecDeque::<Inmate>::new(),
-            }
+                threads: Vec::new() 
+            };
+
+            pp.spool();
+
+            return &mut pp;
         }
 
-        fn spool() {
-
+        fn spool(&mut self) {
+            for i in 0..MAX_THREADS {
+                self.threads.index(0)
+            }
         }
     }
 }
